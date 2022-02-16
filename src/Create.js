ad9f32de5b45657d28851b2490620a1416fa125b
mengantieo <95075858+mengantieo@users.noreply.github.com>
2022-02-16 20:11:44 +0700

    first commit

---
+++ src/Create.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('tarkezo');
+    const [isLoading, setIsLoading] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        const blog = {title, body, author}
+        setIsLoading(true)
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {'Content-Type' : 'application/json'},
+            body: JSON.stringify(blog)
+        })
+        .then(()=>{
+            console.log('new blog added');
+            setIsLoading(false)
+            history.push('/');
+        })
+    }
+
+    return ( 
+        <div className="create">
+            <h2>Add new Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title: </label>
+                <input
+                    required
+                    value={title}
+                    onChange={(e)=> setTitle(e.target.value)}
+                />
+                <label>Blog body: </label>
+                <textarea 
+                    required
+                    value={body}
+                    onChange={(e)=> setBody(e.target.value)}
+                ></textarea>
+                <label>Blog author</label>
+                <select value={author}
+                onChange={(e)=> setAuthor(e.target.value)}>
+                    <option value="tarkezo">Tarkezo</option>
+                    <option value="mario">Mario</option>
+                </select>
+                { !isLoading && <button>Add blog</button>}
+                { isLoading && <button>Adding blog....</button>}
+            </form>
+        </div>
+     );
+}
+ 
+export default Create;
